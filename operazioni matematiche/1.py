83f4931c4aac0a62568dfa94702ded4e1c7b1747
Erik Fakin <e.fakin3@gmail.com>
2024-11-27 13:10:45 +0100

    operazioni

---
+++ operazioni matematiche/1.py
@@ -0,0 +1,59 @@
+#OPERAZIONI CON I NUMERI
+
+
+
+a = 5.6
+b = 3.14
+
+print("a =", a, ", b=", b)
+
+# SOMMA
+c = a + b
+print("La somma tra a e b vale:", c)
+
+print("Aggiungo 2 ad a")
+c = a + 2
+print(c) # c sara uguale a 7.6
+
+c =  c + 3 # c sara uguale a 10.6
+print(c)
+
+# c = c + 3 posso scriverlo in un altro
+# modo.
+
+c += 3 # c sara uguale a 13.6
+print(c)
+
+# DIFFERENZA
+
+c = a - b # c = 2.46
+print("a - b =", c)
+
+# Questi sono 2 modi equivalenti
+# per togliere 1 da c.
+
+# c -=1
+# c = c - 1
+
+# MOLTIPLICAZIONE
+
+c = a * b
+print("a * b = c")
+
+#DIVISIONE
+
+c = a / b
+print("a / b = ",c)
+
+
+
+
+
+
+
+
+
+
+
+
+
