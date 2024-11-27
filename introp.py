57584a3a8718a7281e709f4c6827c66fe34ad8b1
Erik Fakin <e.fakin3@gmail.com>
2024-11-27 12:40:28 +0100

    init

---
+++ introp.py
@@ -0,0 +1,7 @@
+lunghezza = float(input('Inserisci la lunghezza'))
+
+altezza = float(input('Inserisci l\'altezza'))
+
+area = lunghezza * altezza
+
+print("L'area e' uguale a", area)
---
+++ tipi di dati/1.py
@@ -0,0 +1,47 @@
+# Tipi di dati
+
+#int - integer o numero intero
+a = 5
+
+print(type(a))
+
+
+#float - floating point number, numero decimale
+b = 3.14
+
+print(type(b))
+
+#str - stringa o testo
+c = 'testo'
+print(type(c))
+
+#bool - boolean, booleano
+d = True
+print(type(d))
+
+
+#list - collezione di dati
+e = [1,2,3,"Giallo", True, 3.14]
+
+print(type(e))
+
+#dict - ditionary o dizionari
+f = {
+        "nome":"Marco",
+        "eta": 13
+    }
+print(type(f))
+
+
+#tuple -
+g = (1, 3, "blue")
+
+print(type(g))
+
+
+
+
+
+
+
+
