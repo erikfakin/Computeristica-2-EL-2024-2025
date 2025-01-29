815652a3890970e2f20a1405e464da9da6e7dea8
Erik Fakin <e.fakin3@gmail.com>
2025-01-29 13:10:20 +0100

    Esercizio stringhe

--- esercizi/11.py
+++ esercizi/11.py
@@ -2,11 +2,11 @@
 
 # Chiedi all'utente di inserire una frase.
 # Scrivi poi
-# 1) quanto e' lunga la frase
-# 2) la frase letta all'incontrario
-# 3) i primi 5 caratteri della frase
-# 4) gli ultimi 5 caratteri della frase
-# 5) se la frase contiene la parola "Ciao"
+# 1) quanto e' lunga la frase ✓
+# 2) la frase letta all'incontrario ✓
+# 3) i primi 5 caratteri della frase ✓
+# 4) gli ultimi 5 caratteri della frase ✓
+# 5) se la frase contiene la parola "Ciao" ✓
 # 6) sostituisci la lettera "a" nella frase con la lettera "x"
 
 frase = input("Inserisci una frase: ")
@@ -24,6 +24,12 @@ print("GLi ultimi 5 caratteri sono:", ultimi_5)
 
 contiene = frase.find("Ciao")
 if contiene == -1:
-    
+    print("La frase non contiene la parola Ciao")
+else:
+    print("La frase continene la parola Ciao in posizione", contiene)
+
+modificata = frase.replace("a", "x")
+print("Modificata: ", modificata)
+
 
 
